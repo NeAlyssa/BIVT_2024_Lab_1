2caab3ea559339f599f56f39d313cfddd28ecbc7
kadarana <m2403815@edu.misis.ru>
2024-09-10 11:22:21 +0300

    Add files via upload

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -9,7 +9,9 @@ public class Program
     {
         Program program = new Program();
 
-        //program.Task_1_1();
+        program.Task_1_1();
+        Console.WriteLine("Hello world!");
+    
         //program.Task_1_2();
         //program.Task_1_3();
         //program.Task_1_4(0.9);
