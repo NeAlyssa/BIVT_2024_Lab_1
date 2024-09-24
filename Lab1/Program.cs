771e8f4c9d6250059fe2f9ef60a5a4160969e3c0
glazpolina <m2411093@edu.misis.ru>
2024-09-24 12:22:20 +0300

    Add files via upload

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -17,14 +17,14 @@ public class Program
         //program.Task_1_5(0, 2);
         //program.Task_1_6(4);
         //program.Task_1_7();
-        program.Task_1_8();
+        //program.Task_1_8();
         //program.Task_1_9();
         //program.Task_1_10();
         //program.Task_1_11();
         //program.Task_1_12(0.9);
         //program.Task_1_13(-1.5);
         //program.Task_1_14();
-        //program.Task_1_15();
+        program.Task_1_15();
         //program.Task_1_16();
         //program.Task_1_17(10);
         //program.Task_1_18(24);
@@ -158,17 +158,7 @@ public class Program
         int answer = 0;
 
         // code here;
-        int slagaemoe = 1;
-        for (int i = 1; i<=6; i++)
-        {
-            for (int j = 1;j<(i+1); j++)
-            {
-                slagaemoe *= j;
-                
-            }
-            Console.WriteLine(slagaemoe);
-            answer += slagaemoe;
-        }
+        
         // end
         Console.WriteLine(answer);
         return answer;
@@ -185,10 +175,13 @@ public class Program
     }
     public int Task_1_10()
     {
-        int answer = 0;
+        int answer = 1;
 
         // code here
-
+        for (int i=1;i<=7;i++)
+        {
+            answer *= 3;
+        }
         // end
 
         return answer;
@@ -196,18 +189,36 @@ public class Program
     public void Task_1_11()
     {
         // There is no test for this task
+        Console.Write("a)");
+        for (int i=1;i<=5;i++)
+        {
+            Console.Write($"{i} ");
+        }
+        Console.Write("6\n");
 
+        Console.Write("б)");
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"5 ");
+        }
+        Console.Write(5);
         // code here
 
     }
     public double Task_1_12(double x)
     {
         double answer = 0;
-
+        
         // code here
-
+        double p = 1;
+        for(int i=0;i<=10;i++)
+        {
+            if (x == 0) break;
+            answer += 1/ p;
+            p *= x;
+        }
+        answer = Math.Round(answer,2);
         // end
-
         return answer;
     }
     public double Task_1_13(double x)
@@ -215,7 +226,7 @@ public class Program
         double answer = 0;
 
         // code here
-
+        
         // end
 
         return answer;
@@ -223,7 +234,15 @@ public class Program
     public void Task_1_14()
     {
         // There is no test for this task
-
+        int a = 1;
+        int b = 1;
+        for (int i=1;i<=4;i++)
+        {
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            a = a + b;
+            b = a + b;
+        }
         // code here
 
     }
@@ -232,7 +251,19 @@ public class Program
         double answer = 0;
 
         // code here
-
+        int ch = 1;
+        int zn = 1;
+        int n = 1;
+        int a = 1;
+        int b = 1;
+        for (int i=1;i<=5;i++)
+        {
+            n = ch / zn;
+            ch = ch + zn;
+            a = a + b;
+            b = a + b;
+        }
+       
         // end
 
         return answer;
